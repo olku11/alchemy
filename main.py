f074f848181416aa2f6387e0bae9b39228b5899f
olku11 <artem.chastukhin@gmail.com>
2023-03-01 19:14:49 +0300

    second

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    db_session.global_init("db/users.sqlite")
+    db_session.global_init("db/mars_explorer.sqlite")
     session = db_session.create_session()
     user = User()
     user.surname = 'Scott'
